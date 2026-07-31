92e8a39ea07b4088a492234b352bf3afcb2ddff2
Ikram Mughal <ikrammughal91322@gmail.com>
2026-07-31 19:54:05 +0500

    Added hollow square pattern

---
+++ 01_Patterns/02_Hollow_Square.py
@@ -0,0 +1,10 @@
+row = 5
+col = 5
+
+for i in range(row):
+    for j in range(col):
+        if i == 0 or i == row - 1 or j == 0 or j == col - 1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
