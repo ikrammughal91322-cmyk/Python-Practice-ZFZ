4666af9e8d7a2524b4f842bc5a47d711efab8bdc
Ikram Mughal <ikrammughal91322@gmail.com>
2026-08-06 23:28:27 +0500

    Add 01_Hollow_Rectangl.pyn

---
+++ 01_Patterns/02_Rectangle_Patterns/01_Hollow_Rectangle.py
@@ -0,0 +1,9 @@
+n = 5
+m = 7
+for i in range(n):
+    for j in range(m):
+        if (i == 0 or i == n-1) or (j == 0 or j == m-1):
+            print("*",end="  ")
+        else:
+            print(" ",end="  ")
+    print()
