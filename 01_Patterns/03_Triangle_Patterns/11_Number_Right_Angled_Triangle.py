34f64a168e4446f3266550aff1be81570e48ef8a
Ikram Mughal <ikrammughal91322@gmail.com>
2026-08-13 14:01:11 +0500

    16_Right_Aligned_Number_Triangle.py

--- 01_Patterns/03_Triangle_Patterns/11_Number_Right_Angled_Triangle.py
+++ 01_Patterns/03_Triangle_Patterns/11_Number_Right_Angled_Triangle.py
@@ -4,3 +4,4 @@ for i in range(n):
     for j in range(i+1):
         print(j +1,end=" ")
     print()
+    
---
+++ 01_Patterns/03_Triangle_Patterns/16_Right_Aligned_Number_Triangle.py
@@ -0,0 +1,7 @@
+n= 11
+for i in range(n):
+    num = 11
+    for j in range(n-i):
+        print(num ,end=" ")
+        num -= 1
+    print()
